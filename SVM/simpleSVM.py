8252b82802ff2cdfe73390c8ef3f13fd49c9489e
codewyf <1090187390@qq.com>
2020-12-15 17:33:48 +0000

    update svm

---
+++ SVM/simpleSVM.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 15/12/2020 14:58
+@Auth ： Codewyf
+@File ：simpleSVM.py
+@IDE ：PyCharm
+@Motto：Go Ahead Instead of Hesitating
+
+"""
---
+++ SVM/softmargin_with_C.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 15/12/2020 15:00
+@Auth ： Codewyf
+@File ：softmargin_with_C.py
+@IDE ：PyCharm
+@Motto：Go Ahead Instead of Hesitating
+
+"""
